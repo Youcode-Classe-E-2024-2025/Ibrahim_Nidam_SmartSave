8e9bc77ea1d7fb42f428d3239a249aeda3f93e62
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-02-27 10:44:27 +0000

    CONTROLLER: auth controller created with register method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+
+class AuthController extends Controller
+{
+    public function store(){
+        request()->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(8)->max(14), 'confirmed'],
+            'pin' =>['required', 'numeric', 'digits:4']
+        ]);
+
+        $user = User::create([
+            'name' => request()->name,
+            'email' => request()->email,
+            'password' => Hash::make(request()->password)
+        ]);
+
+        Profile::create([
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'profile_pin' => Hash::make(request()->pin),
+            'role' => 'admin'
+        ]);
+        
+        Auth::login($user);
+
+        return redirect('/');
+    }
+}
