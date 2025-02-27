34e9e9deef5a21415946c39745722c11b72b77bf
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-02-27 10:49:18 +0000

    ROUTE: new added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'auth.auth');
+Route::view('/home', 'home');
+
+Route::post('/auth/register', [AuthController::class, 'store']);
+
+Route::post('/auth/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
+
+Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
+Route::post('/profiles', [ProfileController::class, 'store'])->name('profiles.store');
+Route::post('/profiles/verify-pin', [ProfileController::class, 'verifyPin'])->name('profiles.verifyPin');
